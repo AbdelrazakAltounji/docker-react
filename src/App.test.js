55e2eaf20fcaa9ae5a781420c6cad385950a16ab
Abdelrazak ALTOUNJI <abdelrazak.altounji@ibm.com>
2022-03-23 10:59:36 +0100

    changed

--- src/App.test.js
+++ src/App.test.js
@@ -8,6 +8,12 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
+
+it('sum test', () =>{
+  expect(sum(1, 2)).toEqual(3);
+  expect(sum(2, 2)).toEqual(4); 
+});
+
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
